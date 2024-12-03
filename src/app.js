e55899480753d92fa2e1f488ded30ac7280cc958
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-12-03 13:10:41 +0530

    created server with multiple routes and install nodemon for auto refreshing page

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
-console.log("Starting a new project")
+const express = require("express");
 
-console.log("testing")
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from the Home page: csk");
+});
+
+app.get("/login", (req, res) => {
+    res.send("Hello from the Login page");
+});
+
+app.get("/dashboard", (req, res) => {
+    res.send("Hello from the Dashboard page");
+});
+
+app.listen(7777, () => {
+    console.log("Server is successfully listening on port 6000...")
+});

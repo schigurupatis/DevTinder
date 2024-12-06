2a7cbb31b6934adba2f7db7024ee6d0bd001370a
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-12-06 12:37:48 +0530

    working with array of rounte handers

--- src/app.js
+++ src/app.js
@@ -4,17 +4,26 @@ const app = express();
 
 app.use(
     "/user", 
-    (req, res, next)=> {
-        //Route Handler
-        console.log("Handling the route user 1");
-        next()
-        res.send("Route Handler 1");
-    },
-    (req, res) => {
-        //Route Handler
-        console.log("Handling the route user 2")
-        res.send("Route Handler 2");
-    }
+    [
+        (req, res, next)=> {
+            //Route Handler
+            console.log("Handling the route user 1");
+            next()
+            res.send("Route Handler 1");
+        },
+        (req, res, next) => {
+            //Route Handler
+            console.log("Handling the route user 2")
+            next();
+            res.send("Route Handler 2");
+        },
+        (req, res, next) => {
+            //Route Handler
+            console.log("Handling the route user 3")
+            next();
+            //res.send("Route Handler 3");
+        }
+    ]
 
 )
 

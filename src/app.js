474af5ea1e27d0d383c6c3425683f0c97915b44b
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-12-06 19:23:55 +0530

    worked with admin middleware function

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require("express");
 
 const app = express();
 
+// Handle Auth Middleware for all GET, POST, PATCH, DELETE ... requests
+app.use("/admin", (req, res, next) => {
+    //check if the user is authorized or not
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if(!isAdminAuthorized) {
+        res.status(401).send("Not authorized");
+    }else {
+        next();
+    }
+});
+
+app.get("/user", (req, res) => {
+    res.send("User data send")
+})
+
 app.get("/admin/getAllData", (req, res)=> {
-    res.send("All Data Sent")
+    res.send("All Data Sent");
 })
 
 app.get("/admin/deleteUser", (req, res)=> {
-    res.send("Delete User")
+    res.send("Delete User");
 })
 
 app.listen(7777, () => {
